939b161f28b953c60a76ba3118a65fa8d2719327
Daniel Olsson <80417565+DanielSmastadOlsson@users.noreply.github.com>
2021-03-10 22:09:33 +0100

    Update and rename bokare.py to assistant.py
    Change from Swedish to English.

--- assistant.py
+++ assistant.py
@@ -1,15 +1,19 @@
-#Ett program för att lägga till bokare ("assistents") i Zoom.
+# An app to assign assistants with scheduling priviledge 
+# API instructions from Zoom: https://marketplace.zoom.us/docs/api-reference/zoom-api/users/userassistantcreate
+# "Assistants are the users to whom the current user has assigned . These assistants can schedule meeting on behalf of the current user..."
+
 import http.client
 import webbrowser
 from tkinter import *
-progtitle="Bokare i Zoom (V 202103101550)"
-print(progtitle , "startar. / dos@du.se")
+progtitle="Assistants in Zoom (V 202103102205)"
+print(progtitle , ". / smastad@gmail.com")
 root=Tk()
 root.title(progtitle)
-a = Label(root, text="Lägg till assistent/bokare i Zoom.")
+a = Label(root, text="Add assistant in Zoom.")
 
-labelVem = Label(root, text="Vem ska schemalägga?")
-labelwho = Label(root, text="...åt vem?")
+# I use email, like: smastad@gmail.com
+labelVem = Label(root, text="Who will scheduale?")
+labelwho = Label(root, text="...on whos behalf of?")
 labelpass = Label(root, text="JWT Token:")
 eVem= Entry(root)
 eWho= Entry(root)
@@ -29,6 +33,7 @@ ePass.grid(row=rader, column=1)
 rader=rader+1
 
 def klickarToken():
+    # On this page, you will find the Token needed.
     webbrowser.open("https://marketplace.zoom.us/develop/apps/H21m5DTeM4jtnK6LZuuI/credentials")
 
 def klickar():
@@ -38,20 +43,15 @@ def klickar():
     password=ePass.get()
     conn = http.client.HTTPSConnection("api.zoom.us")
 
-    # user=input("Vem ska schemalägga? (användarnamn): ")
-    # touser=input("Åt vem? (användarnamn): ")
-    paytemp = "{\"assistants\":[{\"email\":\"" + user + "@du.se\"}]}"
-    payload = paytemp
+    
+    payload = "{\"assistants\":[{\"email\":\"" + user + "\"}]}"
 
-    # password="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6ImlwM1RCRU5RUTVtZHF2ekNtdklwcnciLCJleHAiOjE2MTUzODEwMDcsImlhdCI6MTYxNTM3NTYwOH0.9WEq2gW_qOozGIQezBa-3xUYXtlrrAvr0SVeJmV-d14"
-    # print("https://marketplace.zoom.us/develop/apps/H21m5DTeM4jtnK6LZuuI/credentials")
-    # password=input("JWT Token: ")
     headers = {
         'content-type': "application/json",
         'authorization': "Bearer" + password
     }
 
-    conn.request("POST", "/v2/users/" + touser + "@du.se/assistants", payload, headers)
+    conn.request("POST", "/v2/users/" + touser + "/assistants", payload, headers)
 
     res = conn.getresponse()
     data = res.read()
@@ -65,8 +65,8 @@ def klickar():
 
 
 
-buttonVerkstall = Button(root, text="Verkställ", command=klickar)
-buttonHitta = Button(root, text="Hitta token", command=klickarToken)
+buttonVerkstall = Button(root, text="Do this now", command=klickar)
+buttonHitta = Button(root, text="Find the token", command=klickarToken)
 buttonVerkstall.grid(row=rader, column=1)
 buttonHitta.grid(row=rader, column=0)
 rader=rader+1
